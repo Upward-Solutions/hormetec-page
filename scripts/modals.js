86f530e747981e6cdee455e9545fe2f1ed47c50c
Juan Manuel CastillO <juanmcastillo3@gmail.com>
2024-07-27 13:06:49 -0300

    details: modals and dekstop

--- scripts/modals.js
+++ scripts/modals.js
@@ -1,19 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const verMasBtns = document.querySelectorAll(".ver-mas-btn");
-    const modales = document.querySelectorAll(".modal");
-    const cerrarBtns = document.querySelectorAll(".cerrar");
-  
-    // Mostrar modal al hacer clic en "Ver más"
-    verMasBtns.forEach((btn, index) => {
-      btn.addEventListener("click", () => {
-        modales[index].style.display = "flex";
-      });
-    });
-  
-    // Cerrar modal al hacer clic en la cruz
-    cerrarBtns.forEach((btn, index) => {
-      btn.addEventListener("click", () => {
-        modales[index].style.display = "none";
-      });
+  const verMasBtns = document.querySelectorAll(".ver-mas-btn");
+  const cerrarBtns = document.querySelectorAll(".cerrar");
+
+  // Mostrar modal al hacer clic en "Ver más"
+  verMasBtns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      const modal = btn.nextElementSibling;
+      modal.style.display = "flex";
     });
   });
+
+  // Cerrar modal al hacer clic en la cruz
+  cerrarBtns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      const modal = btn.closest(".modal");
+      modal.style.display = "none";
+    });
+  });
+});
