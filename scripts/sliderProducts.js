6af67bf2e6c98c6fc718b0eb69a12fd08376405b
Juan Manuel CastillO <juanmcastillo3@gmail.com>
2024-11-07 02:27:01 -0300

    sections and responsive details

--- scripts/sliderProducts.js
+++ scripts/sliderProducts.js
@@ -1,24 +1,34 @@
 let index = 0; // Índice de la página actual
 const itemsToShow = 3; // Número de elementos visibles en el slider
 
+// Función para verificar si estamos dentro del rango de la media query
+function isMobileView() {
+  return window.matchMedia("(min-width: 481px) and (max-width: 767px)").matches;
+}
+
 function moveSlide(step) {
-    const slides = document.querySelectorAll('.slider-item');
-    const totalSlides = slides.length;
+  // Solo ejecutamos el slider si no estamos en la media query de dispositivos móviles
+  if (isMobileView()) {
+    return; // Sale de la función y no hace nada si está en la media query
+  }
 
-    // Calcula cuántas páginas habrá
-    const totalPages = Math.ceil(totalSlides / itemsToShow);
+  const slides = document.querySelectorAll('.slider-item');
+  const totalSlides = slides.length;
 
-    // Actualiza el índice
-    index += step; // Incrementa o decrementa el índice según el paso
+  // Calcula cuántas páginas habrá
+  const totalPages = Math.ceil(totalSlides / itemsToShow);
 
-    // Ajusta el índice para permitir desplazamientos continuos
-    if (index < 0) {
-        index = totalPages - 1; // Regresa a la última página si se intenta retroceder desde la primera
-    } else if (index >= totalPages) {
-        index = 0; // Regresa a la primera página si se intenta avanzar desde la última
-    }
+  // Actualiza el índice
+  index += step; // Incrementa o decrementa el índice según el paso
 
-    // Calcula el desplazamiento en función de la cantidad de elementos que se están mostrando
-    const offset = -index * (itemsToShow * (100 / totalPages)); // Ajuste del desplazamiento
-    document.querySelector('.slider-items').style.transform = `translateX(${offset}%)`;
+  // Ajusta el índice para permitir desplazamientos continuos
+  if (index < 0) {
+    index = totalPages - 1; // Regresa a la última página si se intenta retroceder desde la primera
+  } else if (index >= totalPages) {
+    index = 0; // Regresa a la primera página si se intenta avanzar desde la última
+  }
+
+  // Calcula el desplazamiento en función de la cantidad de elementos que se están mostrando
+  const offset = -index * (itemsToShow * (100 / totalPages)); // Ajuste del desplazamiento
+  document.querySelector('.slider-items').style.transform = `translateX(${offset}%)`;
 }
