cea75179f6105d9b18ebd84d76472319c93172ff
juanmcastillo3 <juanmcastillo3@users.noreply.github.com>
2024-12-03 01:55:19 +0000

    deploy: d4ce1082f008dc4276336a0d54a3b8f4c7a2d398

--- scripts/intro.js
+++ scripts/intro.js
@@ -1,13 +1,20 @@
-        document.addEventListener("DOMContentLoaded", function() {
-            const intro = document.getElementById('intro');
-            const content = document.getElementById('content');
-            const navbar = document.querySelector('.navbar');
-            
-            // Esperar a que termine la animación
-            setTimeout(() => {
-                intro.style.display = 'none';
-                content.style.display = 'block';
-                navbar.classList.remove('hidden');
-                navbar.style.display = 'flex'; // Asegúrate de que el navbar sea visible
-            }, 7000); // Ajusta el tiempo según la duración de tu animación
-        });
+document.addEventListener("DOMContentLoaded", function () {
+    const intro = document.getElementById("intro");
+    const content = document.getElementById("content");
+    const navbar = document.querySelector(".navbar");
+    const scrollArrow = document.querySelector(".scroll-up-arrow");
+    const whatsappIcon = document.querySelector(".whatsapp-icon");
+  
+    // Esperar a que termine la animación
+    setTimeout(() => {
+      intro.style.display = "none"; // Oculta la introducción
+      content.style.display = "block"; // Muestra el contenido principal
+      navbar.classList.remove("hidden");
+      navbar.style.display = "flex"; // Asegúrate de que el navbar sea visible
+  
+      // Mostrar la flecha y el ícono de WhatsApp
+      scrollArrow.classList.remove("hidden");
+      whatsappIcon.classList.remove("hidden");
+    }, 7000); // Ajusta el tiempo según la duración de tu animación
+  });
+  
